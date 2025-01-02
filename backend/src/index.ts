5741a59dfc5528fb45ac6a3e99efc7169febc0a8
Saif <droconos99@gmail.com>
2025-01-02 20:10:15 +0600

    Update: Cors config to support dev client url.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,7 +24,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
    cors({
-      origin: process.env.FRONTEND_URL,
+      origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
       credentials: true,
       optionsSuccessStatus: 200,
    })
